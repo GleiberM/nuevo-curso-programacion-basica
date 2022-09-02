bdf2530c1b48872b7ff8d8b5be6d0a3861a4dd0c
Gleiber Manuel Celis Chinchilla <79201150+GleiberM@users.noreply.github.com>
2022-09-02 06:06:57 -0500

    empezamos mokepon
    hay que corregir el codigo de piedrapapeltijera el combate esta mal

---
+++ mokepon/js/codigo.js
@@ -0,0 +1,6 @@
+
+function seleccionarMascotaJugador() {
+    alert("SELECCIONASTE MASCOTA")
+}
+let botonMascotaJugador = document.getElementById("boton_mascota")
+botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
