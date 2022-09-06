2fdb5919dfc62b361fe6d604f7d731772c3b3494
Gleiber Manuel Celis Chinchilla <79201150+GleiberM@users.noreply.github.com>
2022-09-06 05:41:00 -0500

    seleccion de mascotas

--- mokepon/js/codigo.js
+++ mokepon/js/codigo.js
@@ -1,6 +1,43 @@
 
-function seleccionarMascotaJugador() {
-    alert("SELECCIONASTE MASCOTA")
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById("boton_mascota")
+    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 }
-let botonMascotaJugador = document.getElementById("boton_mascota")
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+function seleccionarMascotaJugador() {
+
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipeyo = document.getElementById('capipeyo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador =document.getElementById('mascota_jugador')
+
+    if (inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML = 'Hipodoge'
+    } else if (inputCapipeyo.checked) {
+        spanMascotaJugador.innerHTML = 'Capipeyo'
+    } else if (inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = 'Ratigueya'
+    } else  {
+        alert('Debes selecionar una mascota')
+    }
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+    let mascotaAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota_enemigo')    
+
+    if (mascotaAleatorio == 1) {
+          spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (mascotaAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = 'Capipeyo'
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+}
+
+function aleatorio(min, max) {
+    return  Math.floor(Math.random()* (max - min + 1) + min)
+}
+
+window.addEventListener('load', iniciarJuego)
