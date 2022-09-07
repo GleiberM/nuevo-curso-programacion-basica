b1978b435b5307e9259de0e36a6deb8a9b1f35e6
Gleiber Manuel Celis Chinchilla <79201150+GleiberM@users.noreply.github.com>
2022-09-07 05:43:42 -0500

    Combate

--- mokepon/js/codigo.js
+++ mokepon/js/codigo.js
@@ -1,7 +1,17 @@
+let ataqueJugador
+let ataqueEnemigo
 
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById("boton_mascota")
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById('boton_fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById('boton_agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById('boton_tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
+
 }
 
 function seleccionarMascotaJugador() {
@@ -34,6 +44,60 @@ function seleccionarMascotaEnemigo() {
     } else {
         spanMascotaEnemigo.innerHTML = 'Ratigueya'
     }
+}
+
+function ataqueFuego() {
+    ataqueJugador = 'FUEGO'
+    ataqueAleatorioEnemigo()
+
+}
+
+function ataqueAgua() {
+    ataqueJugador = 'AGUA'
+    ataqueAleatorioEnemigo()
+
+}
+
+function ataqueTierra() {
+    ataqueJugador = 'TIERRA'
+    ataqueAleatorioEnemigo()
+
+}
+
+function ataqueAleatorioEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+
+    if (ataqueAleatorio == 1){
+        ataqueEnemigo = 'FUEGO'
+    } else if (ataqueAleatorio == 2) {
+        ataqueEnemigo = 'AGUA'
+    } else {
+        ataqueEnemigo = 'TIERRA'
+    }
+    combate()
+}
+
+function combate() {
+    if (ataqueEnemigo === ataqueJugador) {
+        crearMensaje("Empate")
+    }   else if (ataqueJugador === 'FUEGO' && ataqueEnemigo === 'TIERRA') {
+        crearMensaje("Ganaste")
+    }   else if (ataqueJugador === 'AGUA' && ataqueEnemigo === 'FUEGO') {
+        crearMensaje("Ganaste")
+    }   else if (ataqueJugador === 'TIERRA' && ataqueEnemigo === 'AGUA') {
+        crearMensaje("Ganaste")
+    }   else {
+        crearMensaje("Perdiste")
+    }   
+}
+
+function crearMensaje(resultadoCombate) {
+let mensaje = document.getElementById('mensajes')
+
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = 'Tu mascota ataco con ' + ataqueJugador + ', la mascota del enemigo ataco con ' + ataqueEnemigo + ' ' + resultadoCombate
+
+    mensaje.appendChild(parrafo)
 }
 
 function aleatorio(min, max) {
